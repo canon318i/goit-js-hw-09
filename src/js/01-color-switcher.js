f98abd1309543ad9a863ca234dc270793bfd65f0
Oleksii Kononenko <canon318i@gmail.com>
2022-02-13 18:20:10 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const refs = {
+  startButton: document.querySelector('[data-start]'),
+  stopButton: document.querySelector('[data-stop]'),
+};
 
+let timerId = null;
+const bodyRef = document.querySelector('body');
+
+refs.startButton.addEventListener('click', onStartClick);
+refs.stopButton.addEventListener('click', onStopClick);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function colorCarousel(ref) {
+  ref.style.backgroundColor = getRandomHexColor();
+}
+
+function onStartClick() {
+  timerId = setInterval(colorCarousel, 1000, bodyRef);
+  refs.startButton.disabled = true;
+  refs.stopButton.disabled = false;
+}
+
+function onStopClick() {
+  clearInterval(timerId);
+  refs.stopButton.disabled = true;
+  refs.startButton.disabled = false;
+}
